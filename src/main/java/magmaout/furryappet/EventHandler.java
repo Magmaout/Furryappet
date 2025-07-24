f89d9060bb5a9746470bd11b3edf7a78839913d6
MRkto <koteypro577@gmail.com>
2025-07-24 18:23:42 +0300

    revert add lastKnowName to FurryappetCapability

--- src/main/java/magmaout/furryappet/EventHandler.java
+++ src/main/java/magmaout/furryappet/EventHandler.java
@@ -28,7 +28,7 @@ public class EventHandler {
     @SubscribeEvent
     public void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
         if (event.getObject() instanceof EntityPlayerMP) {
-            event.addCapability(new ResourceLocation(Furryappet.MODID, "uwu"), new FurryappetCapability((EntityPlayerMP) event.getObject()));
+            event.addCapability(new ResourceLocation(Furryappet.MODID, "uwu"), new FurryappetCapability());
         }
     }
 }
